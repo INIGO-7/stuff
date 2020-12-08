db2cd284455f7f6ad35698b96a22746961706052
INIGO-7 <inigofernandez@opendeusto.es>
2020-12-08 22:34:04 +0100

    Jmenus que funcionan con mnemonics (activamos pulsando Alt+"tecla").

--- stuff/src/swing/Window.java
+++ stuff/src/swing/Window.java
@@ -3,11 +3,16 @@ package swing;
 import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 
 public class Window extends JFrame{
 
 	public int width, height;
 	public String title;
+	
+	//swing
+	private JMenu menuE, menuF;
 	
 	public Window(int width, int height, String title) {
 		
@@ -25,6 +30,13 @@ public class Window extends JFrame{
 		
 //		setLocationRelativeTo(null); //para que la ventana esté en el centro
 		
+		JMenuBar menuBar = new JMenuBar();
+		menuE = new JMenu( "Presiona Alt+E" ); menuE.setMnemonic( KeyEvent.VK_E );
+		menuBar.add( menuE );
+		menuF = new JMenu( "Presiona Alt+F" ); menuF.setMnemonic( KeyEvent.VK_F );
+		menuBar.add( menuF );
+		
+		setJMenuBar( menuBar );
 		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
