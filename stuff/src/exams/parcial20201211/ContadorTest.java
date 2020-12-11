7779422c4ceb514f6dcb4271b8ffd46c1b0f6c8d
INIGO-7 <inigofernandez@opendeusto.es>
2020-12-11 09:16:34 +0100

    Clase contadorTest con tests creados.

---
+++ stuff/src/exams/parcial20201211/ContadorTest.java
@@ -0,0 +1,32 @@
+package exams.parcial20201211;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ContadorTest {
+	
+	@Test
+	public void testGet() {
+		Contador cont = new Contador(0);
+		assertEquals(0, cont.get());
+	}
+
+	@Test
+	public void testInc() {
+		Contador cont = new Contador(0);
+		assertEquals(0, cont.get());
+		cont.inc();
+		assertEquals(1, cont.get());
+	}
+	
+	@Test
+	public void testIncNumero() {
+		Contador cont = new Contador(3);
+		cont.inc(4);
+		assertEquals(7, cont.get());
+	}
+
+
+
+}
