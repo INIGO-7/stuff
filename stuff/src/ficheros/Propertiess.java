5f2e6296d34cd0c94f4af27f2b6dcc5e0c65e03f
Iñigo Fernandez <inigofernandez@opendeusto.es>
2021-01-05 10:00:01 +0100

    Prueba con float

--- stuff/src/ficheros/Propertiess.java
+++ stuff/src/ficheros/Propertiess.java
@@ -21,10 +21,12 @@ public class Propertiess {
 			
 			
 			int tlf = Integer.parseInt(properties.getProperty("telefono")); //para conseguir el numero de telefono como int en vez de string
+			float flData = Float.parseFloat(properties.getProperty("floatData")); //prueba para ver si funciona con valores float 
 			
 			System.out.println(properties.getProperty("nombre"));
 			System.out.println(properties.getProperty("apellido"));
 			System.out.println(tlf);
+			System.out.println(flData);
 			System.out.println(properties.getProperty("direccion"));
 			
 			properties.setProperty("nombre", "Alberto");
