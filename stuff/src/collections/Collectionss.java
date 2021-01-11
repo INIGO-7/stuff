4daff6aa97f393602b1ea23385e69f8a130cf18d
INIGO-7 <inigofernandez@opendeusto.es>
2021-01-11 17:20:17 +0100

    uso de Collections.max, min... y cambio en la clase Arraylist

--- stuff/src/collections/Collectionss.java
+++ stuff/src/collections/Collectionss.java
@@ -2,20 +2,19 @@ package collections;
 
 import java.util.Collections;
 
-import collections.Map.Hashmap;
+import collections.List.Arraylist;
 
 public class Collectionss {
 
 	public static void main(String[] args) {
 		
-		Hashmap hs = new Hashmap();						//clase Hashmap
+		Arraylist arl = new Arraylist();						//clase Arraylist, no confundir con colección ArrayList<>
 		
-		//Collections.max(hs.getMap());
+		System.out.println("Maximum value of arraylist is: " + Collections.max(arl.getArraylist()));
+		System.out.println("Minimum value of arraylist is: " + Collections.min(arl.getArraylist()));
 		
-		
-		
-		
-		
+		System.out.println(Collections.binarySearch(arl.getArraylist(), 30));			//binarySearch devuelve el index del elemento buscado si lo encuentra, y si no devuelve (-(insertion point) - 1)
+																						//que es -(el index en el que se metería el elemento en nuestra coleccion) - 1	
 		
 		
 	}
--- stuff/src/collections/List/Arraylist.java
+++ stuff/src/collections/List/Arraylist.java
@@ -3,62 +3,76 @@ package collections.List;
 import java.util.*; 
 
 public class Arraylist { 
-    public static void main(String[] args) 
-    { 
-    	
-//        int n = 3; 
-//  
-//        // aList is an ArrayList of ArrayLists 
-//        ArrayList<ArrayList<Integer>> masterArrayList =  new ArrayList<ArrayList<Integer> >(n); 
-//        
-//        
-//        // Create n lists one by one and append to the  
-//        // master list (ArrayList of ArrayList) 
-//        ArrayList<Integer> a1 = new ArrayList<Integer>(); 
-//        a1.add(1); 
-//        a1.add(2); 
-//        masterArrayList.add(a1); 
-//  
-//        ArrayList<Integer> a2 = new ArrayList<Integer>(); 
-//        a2.add(5); 
-//        masterArrayList.add(a2); 
-//  
-//        ArrayList<Integer> a3 = new ArrayList<Integer>(); 
-//        a3.add(10); 
-//        a3.add(20); 
-//        a3.add(30); 
-//        masterArrayList.add(a3); 
-//  
-//        for (int i = 0; i < masterArrayList.size(); i++) { 
-//            for (int j = 0; j < masterArrayList.get(i).size(); j++) { 
-//                System.out.print(masterArrayList.get(i).get(j) + " "); 
-//            } 
-//            System.out.println(); 
-//        }
-        
-        ArrayList<ArrayList<Integer>> masterArrayList = new ArrayList<>();
+	
+	private ArrayList<ArrayList<Integer>> masterArrayList; 
+	ArrayList<Integer> a1, a2, a3;
+	
+	public Arraylist() {
+//      int n = 3; 
+//
+//      // aList is an ArrayList of ArrayLists 
+//      ArrayList<ArrayList<Integer>> masterArrayList =  new ArrayList<ArrayList<Integer> >(n); 
+//      
+//      
+//      // Create n lists one by one and append to the  
+//      // master list (ArrayList of ArrayList) 
+//      ArrayList<Integer> a1 = new ArrayList<Integer>(); 
+//      a1.add(1); 
+//      a1.add(2); 
+//      masterArrayList.add(a1); 
+//
+//      ArrayList<Integer> a2 = new ArrayList<Integer>(); 
+//      a2.add(5); 
+//      masterArrayList.add(a2); 
+//
+//      ArrayList<Integer> a3 = new ArrayList<Integer>(); 
+//      a3.add(10); 
+//      a3.add(20); 
+//      a3.add(30); 
+//      masterArrayList.add(a3); 
+//
+//      for (int i = 0; i < masterArrayList.size(); i++) { 
+//          for (int j = 0; j < masterArrayList.get(i).size(); j++) { 
+//              System.out.print(masterArrayList.get(i).get(j) + " "); 
+//          } 
+//          System.out.println(); 
+//      }
       
-        
-	    ArrayList<Integer> a1 = new ArrayList<Integer>(); 
-	    a1.add(1); 
-	    a1.add(2); 
-	    masterArrayList.add(a1); 
+      masterArrayList = new ArrayList<>();
+    
+      
+      a1 = new ArrayList<Integer>(); 
+	  a1.add(1); 
+	  a1.add(2); 
+	  masterArrayList.add(a1); 
 	
-	    ArrayList<Integer> a2 = new ArrayList<Integer>(); 
-	    a2.add(5); 
-	    masterArrayList.add(a2); 
+	  a2 = new ArrayList<Integer>(); 
+	  a2.add(5); 
+	  masterArrayList.add(a2); 
 	
-	    ArrayList<Integer> a3 = new ArrayList<Integer>(); 
-	    a3.add(10); 
-        a3.add(20); 
-        a3.add(30); 
-        masterArrayList.add(a3); 
+	  a3 = new ArrayList<Integer>(); 
+	  a3.add(10); 
+      a3.add(20); 
+      a3.add(30); 
+      masterArrayList.add(a3); 
 
-        for (int i = 0; i < masterArrayList.size(); i++) { 
-            for (int j = 0; j < masterArrayList.get(i).size(); j++) { 
-                System.out.print(masterArrayList.get(i).get(j) + " "); 
-            } 
-            System.out.println(); 
-        }
+      for (int i = 0; i < masterArrayList.size(); i++) { 
+          for (int j = 0; j < masterArrayList.get(i).size(); j++) { 
+              System.out.print(masterArrayList.get(i).get(j) + " "); 
+          } 
+         
+      }
+      
+      
+	}
+	
+	public ArrayList<Integer> getArraylist() {
+		return a3;
+	}
+	
+    public static void main(String[] args) { 
+    
+    	Arraylist al = new Arraylist();
+    	
     } 
 }
