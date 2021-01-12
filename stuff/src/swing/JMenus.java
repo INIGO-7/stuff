8c7cf9bd66638a714b20c2e4ba969b4a3f21fb16
INIGO-7 <inigofernandez@opendeusto.es>
2021-01-12 08:41:34 +0100

    JMenuItems añadidos con actionListeners.

--- stuff/src/swing/JMenus.java
+++ stuff/src/swing/JMenus.java
@@ -1,5 +1,7 @@
 package swing;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 import javax.swing.*;
@@ -8,6 +10,8 @@ public class JMenus {
 	
 	public static void main(String[] args) {
 	
+		JMenuItem item1, item2;
+		
 		JMenu menuE;
 		JMenu menuF;
 		JMenuBar menuBar = new JMenuBar();
@@ -16,9 +20,34 @@ public class JMenus {
 		
 		menuBar = new JMenuBar();
 		
+		item1 = new JMenuItem("Hola");
+		item2 = new JMenuItem("Programador");
+		
+		item1.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.print("Hola ");
+			}
+			
+		});
+		
+		item2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.print("programador.");
+			}
+			
+		});
+		
 		menuE = new JMenu("press Alt+E"); menuE.setMnemonic(KeyEvent.VK_E);
-		menuBar.add(menuE);
+		menuE.add(item1);
+		
 		menuF = new JMenu("press Alt+F"); menuF.setMnemonic(KeyEvent.VK_F);
+		menuF.add(item2);
+		
+		menuBar.add(menuE);
 		menuBar.add(menuF);
 		
 		window.setJMenuBar( menuBar );
